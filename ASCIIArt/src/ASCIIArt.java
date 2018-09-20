f9806cb9cf23efafafcb179a785e71ca5b85ee70
s-varmaa <s-varmaa@53S500038297.it.bsd405.org>
2018-09-20 15:11:41 -0700

    Turning in Project #1.

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -15,3 +15,103 @@
  */
 
 // YOUR CODE GOES HERE :)
+
+public class ASCIIArt {
+	public static void main(String args []) {
+		pumpkinStem ();
+		System.out.println();
+		System.out.println("           ______|\"\"|______    ");
+		System.out.println("          /      |\"\"|      \\  ");
+		System.out.println("         /    _        _    \\   ");
+		System.out.println("        /    /_\\      /_\\    \\");
+		System.out.println("       /           _          \\ ");
+		System.out.println("       |          /_\\         | ");
+		System.out.println("       |                      |  ");
+		System.out.println("       \\    __   ____   __    / ");
+		System.out.println("        \\   \\ |_|    |_| /   / ");
+		System.out.println("         \\   \\__________/   /  ");
+		System.out.println("          \\________________/    ");
+		System.out.println("\n");
+	
+		happy ();
+		halloween();
+
+	}
+	
+	public static void pumpkinStem () {
+		System.out.print("                 |\"\"|          ");
+	}
+	
+	public static void letterH () {
+		System.out.println(" |__| ");
+		System.out.println(" |  | ");
+	}
+	
+	public static void letterA () {
+		System.out.println("  _");
+		System.out.println(" /_\\");
+		System.out.println("/   \\");
+	}
+	
+	public static void letterP () {
+		System.out.println("  __");
+		System.out.println(" |__)");
+		System.out.println(" | ");
+	}
+	
+	public static void letterE() {
+		System.out.println("  ___");
+		System.out.println(" |___");
+		System.out.println(" |___ ");
+	}	
+	
+	public static void letterL() {
+		System.out.println();
+		System.out.println(" |");
+		System.out.println(" |___ ");
+	} 
+	
+	public static void happy () {
+		letterH ();
+		letterA ();
+		letterP ();
+		letterP ();
+		System.out.println();
+		System.out.println(" \\_/");
+		System.out.println("  |");
+	}
+	
+	public static void halloween () {
+		System.out.println("\n");
+		letterH();
+		letterA ();
+		letterL ();
+		letterL ();
+		System.out.println("  ___ ");
+		System.out.println(" /   \\");
+		System.out.println(" \\___/ \n");
+		System.out.println(" \\    /");
+		System.out.println("  \\/\\/");
+		letterE();
+		letterE();
+		System.out.println(" \n |\\ |");
+		System.out.println(" | \\|");
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
